1839fa78f2491162970b3c046bfcb75860fa0f06
Eligarf <eligarf@live.com>
2023-01-07 12:55:09 -0800

    First stab

--- scripts/systems/pf2e.js
+++ scripts/systems/pf2e.js
@@ -1,60 +1,92 @@
 import { Stealthy, StealthyBaseEngine } from '../stealthy.js';
 
-export class StealthyPF2e extends StealthyBaseEngine {
+export class StealthyPf2e extends StealthyBaseEngine {
 
   constructor() {
-    // Hook the relevant skills to capture spot and hidden test
-    // results into effects on the actor.
     super();
-    console.warn(`Stealthy for '${game.system.id}' is stubbed out, needs development`);
+
+    // There is probably a better practice for figuring out skill checks in PF2E, but this "works"
+    Hooks.on('createChatMessage', async (message, options, id) => {
+      if (message.flavor.includes('>Skill Check: Stealth<')) {
+        await this.rollStealth(message, options, id);
+      }
+      else if (message.flavor.includes('>Perception Check<')) {
+        await this.rollPerception(message, options, id);
+      }
+    });
+  }
+
+  findHiddenEffect(actor) {
+    return actor.getFlag(Stealthy.MODULE_ID, 'hidden');
+  }
+
+  findSpotEffect(actor) {
+    return actor.getFlag(Stealthy.MODULE_ID, 'spot');
   }
 
   isHidden(visionSource, hiddenEffect, target, config) {
-    // Implement your system's method for testing spot data vs hidden data
-    // This should would in the absence of a spot effect on the viewer, using
-    // a passive or default value as necessary
+    const source = visionSource.object?.actor;
+    const stealth = hiddenEffect ?? (10 + target.system.skills.ste.value);
+    const spotEffect = this.findSpotEffect(source);
+    const perception = spotEffect ?? (10 + target.system.attributes.perception.value);
+
+    if (perception <= stealth) {
+      Stealthy.log(`${visionSource.object.name}'s ${perception} can't see ${config.object.name}'s ${stealth}`);
+      return true;
+    }
     return false;
   }
 
   makeHiddenEffectMaker(label) {
-    console.error(`'${game.system.id}' can't make a Hidden effect maker. Heavy lifting goes here.`);
+    return (flag, source) => null;
   }
 
   makeSpotEffectMaker(label) {
-    console.error(`'${game.system.id}' can't make a Spot effect maker. Heavy lifting goes here.`);
+    return (flag, source) => null;
   }
 
-  async updateOrCreateEffect({ label, actor, flag, makeEffect }) {
-    console.error(`'${game.system.id}' isn't compatible with Active Effect use. Heavy lifting goes here.`);
+  async updateOrCreateEffect({ label, actor, flag, source, makeEffect }) {
+    for (const [key, value] of Object.entries(flag)) {
+      await actor.setFlag(Stealthy.MODULE_ID, key, value);
+      break;
+    }
   }
 
-  getHiddenFlagAndValue(actor, hidden) {
-    // Return the data necessary for storing data about hidden, and the
-    // value that should be shown on the token button input
-    return { flag: { hidden: undefined }, value: undefined };
+  getHiddenFlagAndValue(actor, effect) {
+    const value = effect ?? (10 + actor.system.skills.ste.value);
+    return { value };
   }
 
   async setHiddenValue(actor, effect, flag, value) {
-    // If the hidden value was changed, do what you need to store it
-    flag.hidden = value;
-    effect.flags.stealthy = flag;
-    await actor.updateEmbeddedDocuments('ActiveEffect', [effect]);
+    await actor.setFlag(Stealthy.MODULE_ID, 'hidden', value);
   }
 
-  getSpotFlagAndValue(actor, spot) {
-    // Return the data necessary for storing data about spot, and the
-    // value that should be shown on the token button input
-    return { flag: { spot: undefined }, value: undefined };
+  getSpotFlagAndValue(actor, effect) {
+    const value = effect ?? (10 + actor.system.attributes.perception.value);
+    return { value };
   }
 
   async setSpotValue(actor, effect, flag, value) {
-    // If the spot value was changed, do what you need to store it
-    flag.spot = value;
-    effect.flags.stealthy = flag;
-    await actor.updateEmbeddedDocuments('ActiveEffect', [effect]);
+    await actor.setFlag(Stealthy.MODULE_ID, 'spot', value);
+  }
+
+  async rollPerception(message, options, id) {
+    Stealthy.log('rollPerception', { message, options, id });
+
+    const token = canvas.tokens.get(message.speaker.token);
+    const actor = token.actor;
+    await this.updateOrCreateSpotEffect(actor, { spot: Number(message.content) });
+  }
+
+  async rollStealth(message, options, id) {
+    Stealthy.log('rollStealth', { message, options, id });
+
+    const token = canvas.tokens.get(message.speaker.token);
+    const actor = token.actor;
+    await this.updateOrCreateHiddenEffect(actor, { hidden: Number(message.content) });
   }
 }
 
 Hooks.once('init', () => {
-  Stealthy.RegisterEngine('pf2e', () => new StealthyPF2e());
+  Stealthy.RegisterEngine('pf2e', () => new StealthyPf2e());
 });
